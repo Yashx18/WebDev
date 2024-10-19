7fa5e922fd73fb53538c7347359c5627de08f75c
Ken <yash20004x17@gmail.com>
2024-10-19 12:57:28 +0530

    new files added in WebDev Folder

---
+++ Practice/layout 7/script.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body')
+const bottom = document.querySelector('.bottom')
+const upper = document.querySelector('.upper')
+const dark_m = document.querySelector('.dark-mode')
+
+function toggleDarkMode(){
+    document.body.classList.toggle('dark-mode');
+}

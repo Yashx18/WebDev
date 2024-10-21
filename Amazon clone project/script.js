1179660420ddd3a62b9464a7f88140f8665e6296
Ken <yash20004x17@gmail.com>
2024-10-21 17:36:22 +0530

    Learning JS from Start

---
+++ Amazon clone project/script.js
@@ -0,0 +1,14 @@
+const accountId = 18112004;
+let accountEmail = "yashx024@gmail.com";
+var accountPassword = "93549387YH";
+accountCity = "Tokyo";
+
+// console.table(accountId , accountEmail, accountPassword, accountCity);
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+
+alert("Hello User")
